e7832cbc46b7bfa92aad2fea53fd37cdbc8ac72c
Pablo Coletti <95883408+PabloColetti@users.noreply.github.com>
2025-11-30 22:45:09 -0300

    feat: Add initial setup for ms-banks microservice with Maven wrapper, configuration files, and basic application structure.

---
+++ ms-banks/src/test/java/com/santander/challenge/ms_banks/MsBanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.challenge.ms_banks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
