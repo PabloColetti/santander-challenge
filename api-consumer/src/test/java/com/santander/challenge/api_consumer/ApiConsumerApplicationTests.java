60f11e740b0f5503f2fa84f1c8c7c96e514c6a7e
Pablo Coletti <95883408+PabloColetti@users.noreply.github.com>
2025-11-30 21:39:33 -0300

    feat: Add initial setup for API Consumer.

---
+++ api-consumer/src/test/java/com/santander/challenge/api_consumer/ApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.challenge.api_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
