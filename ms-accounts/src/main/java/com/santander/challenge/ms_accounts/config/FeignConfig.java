8131031f128b39f68951cb3f621097c153499971
Pablo Coletti <95883408+PabloColetti@users.noreply.github.com>
2025-12-04 02:03:58 -0300

    feat: Add Feign client configuration classes for ms-accounts and ms-banks

---
+++ ms-accounts/src/main/java/com/santander/challenge/ms_accounts/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.santander.challenge.ms_accounts.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuración para Feign Clients.
+ */
+@Configuration
+@EnableFeignClients(basePackages = "com.santander.challenge.ms_accounts.adapter")
+public class FeignConfig {
+    // Configuración adicional de Feign si es necesaria
+}
+
